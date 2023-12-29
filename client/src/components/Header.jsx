fe966ece0e37119da8510453fcb2bca4eb016747
felipesousac <claudioflsousa@gmail.com>
2023-12-28 22:45:08 -0300

    Set up MessageBoard component to render dynamic messages

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -2,8 +2,8 @@ import "../styles/main.css";
 
 const Header = () => {
   return (
-    <header className="w-full h-12 bg-slate-800 text-left shadow-lg mb-4">
-      <h1 className="size-6 w-auto text-slate-200 font-semibold">
+    <header className="w-full h-12 bg-slate-800 text-left shadow-lg mb-4 flex items-center">
+      <h1 className="size-6 w-auto text-slate-200 font-semibold ml-4">
         Mini Message Board
       </h1>
     </header>
--- client/src/components/MessageBoard.jsx
+++ client/src/components/MessageBoard.jsx
@@ -1,15 +1,46 @@
 import "../styles/main.css";
-import Message from "./Message";
 
 const MessageBoard = () => {
+  const messages = [
+    {
+      text: "Hi there!",
+      user: "Amando",
+      added: new Date(),
+    },
+    {
+      text: "Hello World!",
+      user: "Charles",
+      added: new Date(),
+    },
+    {
+      text: "Hi there!",
+      user: "Bibi",
+      added: new Date(),
+    },
+    {
+      text: "Hello World!",
+      user: "Perigosa",
+      added: new Date(),
+    },
+  ];
+
   return (
     <>
       <div class="w-9/12 border-solid shadow-lg mt-1 p-3 bg-white rounded-lg">
-        <h6 class="pb-2 mb-1 border-b border-gray-200">Messages</h6>
-        <Message />
-        <Message />
-        <Message />
-        <Message />
+        <h6 class="pb-2 mb-1 border-b border-gray-200 font-medium">Messages</h6>
+        {messages.map((message) => (
+          <div class="pt-3">
+            <p class="pb-3 mb-0 small lh-125 border-bottom border-gray border-b border-gray-200">
+              <div className="flex items-center justify-between">
+                <strong class="text-gray-500 block">{message.user}</strong>
+                <span className="text-xs text-gray-400 block">
+                  {message.added.toLocaleDateString()}
+                </span>
+              </div>
+              {message.text}
+            </p>
+          </div>
+        ))}
       </div>
     </>
   );
